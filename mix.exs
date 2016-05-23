ce5a6038d841a5aef2ddefca5b1509343cb74232
Steven Nunez <steven.nunez@gmail.com>
2016-05-22 20:55:23 -0400

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -22,7 +22,7 @@ defmodule JaSerializer.Mixfile do
   defp deps do
     [{:inflex, "~> 1.4"},
      {:plug, "> 1.0.0"},
-     :ecto, "~> 2.0.0-rc"},
+     {:ecto, "~> 2.0.0-rc"},
      {:poison, "~> 1.4 or ~> 2.0"},
      {:earmark, "~> 0.1", only: :dev},
      {:inch_ex, "~> 0.4", only: :docs},
